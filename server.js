447599f2764a133aaeb0f960d3184df0afe860c3
VimalRaj571 <Vimalraj.ktm95@gmail.com>
2017-02-13 22:00:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,56 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+    'article-one' :{
+    title : 'art-1',
+    date : 'Feb-1',
+    cont :`<p>Some stuff in article-1`
+    },
+    'article-two' :{
+    title : 'art-2',
+    date : 'Feb-2',
+    cont :`<p>Some stuff in article-2`
+    },
+    'article-three' :{
+    title : 'art-3',
+    date : 'Feb-3',
+    cont :`<p>Some stuff in article-3`
+    },
+};
 
+function createTemp(data){
+    var title = data.title;
+    var date = data.date;
+    var cont = data.cont;
+    var htmlTemp=
+        `<!DOCTYPE html>
+        <html>
+            <head>
+                <title>${title}</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="/ui/style.css" rel="stylesheet" />
+            </head>
+            <body>
+                <div class="stl">
+                <div>
+                    <a href="/" >Home</a>
+                </div>
+                <hr>
+                <h3>SOmethin</h3>
+                <div>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+                </div>
+                </div>
+            </body>
+        </html>`;
+return htmlTemp
+}
 
 
 app.get('/', function (req, res) {
@@ -25,7 +74,8 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  var articleName = req.params.articleName;
+  res.send(createTemp(articles[articleName]));   //In here "articles" obj names data=articleName=art1 or art2 or art3 
 });
 
 
