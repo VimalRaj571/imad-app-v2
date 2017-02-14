f20b905c56ba4fd5d4b770cbcd76d8c1bae9ebcd
VimalRaj571 <Vimalraj.ktm95@gmail.com>
2017-02-14 21:29:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,13 +4,13 @@ var img = document.getElementById('mini');
 
 //make anim try put soome non static msg
 var marginLeft= 0 ;
-function moveRight(){
+function moveRight(){                                                           //func1 to make add leftmargin ++
     marginLeft = marginLeft + 1 ;
     img.style.marginLeft =  marginLeft +'px';
     //img.style.marginLeft = '400px';
     
         }
 
-img.onclick = function(){
+img.onclick = function(){                                                       //func2 to make call the leftmargin ++
     var animate = setInterval(moveRight,50); // call moveRight in 20msec
 };
