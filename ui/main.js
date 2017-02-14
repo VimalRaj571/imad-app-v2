2791c8cc31e374145b83a3137439f64fb0c2bc9e
VimalRaj571 <Vimalraj.ktm95@gmail.com>
2017-02-14 21:15:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,12 @@ var img = document.getElementById('mini');
 
 //make anim try put soome non static msg
 
-img.onclick = function(){
+function moveRight(){
+    img.style.left = parseInt(img.style.left) + 10 + 'px';
+    //img.style.marginLeft = '400px';
     
-    img.style.marginLeft = '400px';
+        }
+
+img.onclick = function(){
+    animate = setTimeout(moveRight,20); // call moveRight in 20msec
 };
