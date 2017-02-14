db17d2045ac937e6ab574741c73702c4a5cafa59
VimalRaj571 <Vimalraj.ktm95@gmail.com>
2017-02-14 21:06:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,8 @@ console.log('Loaded!');
 
 var img = document.getElementById('mini');
 
+//make anim try put soome non static msg
+
 img.onclick = function(){
     
     img.style.marginLeft = '400px';
