84b8b63975d01afdd361fe0d38b9968b8c4e07ec
VimalRaj571 <Vimalraj.ktm95@gmail.com>
2017-02-14 21:18:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var img = document.getElementById('mini');
 //make anim try put soome non static msg
 
 function moveRight(){
-    img.style.left = parseInt(img.style.left) + 10 + 'px';
+    img.style.marginLeft =  marginLeft + '1' +'px';
     //img.style.marginLeft = '400px';
     
         }
