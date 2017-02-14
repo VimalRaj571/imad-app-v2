db8ffbe5cff6ab817f6a06cf0dfe8993afc27792
VimalRaj571 <Vimalraj.ktm95@gmail.com>
2017-02-14 22:25:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,10 +13,18 @@ img.onclick = function(){                                                       
     var animate = setInterval(moveRight,50); // call moveRight in 20msec
 };
 
-
+//Counter CODE
 var btn = document.getElementById('btn');
 var span = document.getElementById('span_no');
+var counter = 0;
 
 btn.onclick = function(){
-    span.innerHTML = 'dude';
+    
+//Make the request to counter Endpoint
+
+//Capture the response and store in the variable
+
+//Render in the correct span
+    counter = counter + 1;
+    span.innerHTML = counter.toString();
 };
