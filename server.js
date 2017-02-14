621a85f8fe40814cb967c141948ebd41b8920ba8
VimalRaj571 <Vimalraj.ktm95@gmail.com>
2017-02-14 21:53:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ return htmlTemp ;
 var counter = 0;
 app.get('/counter', function (req, res) {
   counter = counter+1 ; 
-  res.send(counter.toString(counter));
+  res.send(counter.toString());
 });
 
 
