a2ef86ee47097bb100fbc1673beab4acffaba45d
VimalRaj571 <Vimalraj.ktm95@gmail.com>
2017-02-14 20:54:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 
 var img = document.getElementById('mini');
 
-img.onclick = function onclk(){
+img.onclick = function(){
     
     img.style.backgroundPosition = '-40px 0px';
 };
