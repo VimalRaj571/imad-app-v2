07e80cb286a74a669309e09197fde69024d71cb5
VimalRaj571 <Vimalraj.ktm95@gmail.com>
2017-02-15 21:48:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,16 +5,6 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var names = [];                                                                 //P.S = Check the Name and Names variable NAMES 
-app.get('/submit-names', function(req, res){    // URL like this /submit-name?name=xxxx
-    //Get the name from request OBJ
-    var name = req.query.name;         //To use the req.query.name insted of req.params.name
-    
-    names.push(name);   //names is array [] and the name is rq URL values like this /submit-names/somenames
-    //JSON = Javascript Object Notation
-    res.send(JSON.stringify(names));
-});
-
 var articles = {
     'article-one' :{
     title : 'art-1',
@@ -89,6 +79,15 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var names = [];                                                                 //P.S = Check the Name and Names variable NAMES 
+app.get('/submit-names', function(req, res){    // URL like this /submit-name?name=xxxx
+    //Get the name from request OBJ
+    var name = req.query.name;         //To use the req.query.name insted of req.params.name
+    
+    names.push(name);   //names is array [] and the name is rq URL values like this /submit-names/somenames
+    //JSON = Javascript Object Notation
+    res.send(JSON.stringify(names));
+});
 
 app.get('/:articleName', function (req, res) {
   var articleName = req.params.articleName;
