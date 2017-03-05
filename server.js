8556ccce46128b017ef520370f1fc26efac02882
Vimal <Vimalraj.ktm95@gmail.com>
2017-03-05 11:43:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,6 +15,21 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
+
+var pool = new Pool(config);
+app.get('/test-db' ,function(req, res){
+   //the DB check
+   //And return thee response
+    pool.query("SELECT * FROM test",function (err ,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else{
+           res.send(JSON.stringify(result));
+       }
+    });
+});
+
+
 var articles = {
     'article-one' :{
     title : 'art-1',
@@ -89,19 +104,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-
-var pool = new Pool(config);
-app.get('/test-db' ,function(req, res){
-   //the DB check
-   //And return thee response
-    pool.query("SELECT * FROM test",function (err ,result){
-       if(err){
-           res.status(500).send(err.toString());
-       } else{
-           res.send(JSON.stringify(result));
-       }
-    });
-});
 
 
 //submit-names
