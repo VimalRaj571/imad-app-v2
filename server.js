0f0cce64c9b2ea69677ca594b278308fd7d0d6b1
Vimal <Vimalraj.ktm95@gmail.com>
2017-03-10 12:45:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,17 +79,18 @@ app.get('/', function (req, res) {
 });
 
 
-var pool = new Pool(config);
+//var pool = new Pool(config);
 app.get("/test-db" ,function(req, res){
+   res.send('Hi');
    //the DB check
    //And return thee response
-    pool.query('SELECT * FROM test',function (err, result){
-       if(err){
-           res.status(500).send(err.toString());
-       } else{
-           res.send(JSON.stringify(result));
-       }
-    });
+   // pool.query('SELECT * FROM test',function (err, result){
+    //   if(err){
+    //       res.status(500).send(err.toString());
+    //   } else{
+    //       res.send(JSON.stringify(result));
+    //   }
+    //});
 });
 
 
