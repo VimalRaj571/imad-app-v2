e8f28c42e2c8a1ad73f00e437c23efdf84bd2706
Vimal <Vimalraj.ktm95@gmail.com>
2017-03-11 19:04:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,13 +63,14 @@ app.get('/', function (req, res) {
 });
 
 
-function hash(input){
+function hash(input , salt){
     //How do we create a hash
-    var hashed = crypto.pbkdf2Sync();
+    var hashed = crypto.pbkdf2Sync(input ,salt ,10000 ,512 ,'sha512');
+    return hashed.toString('hex');
 }
 
 app.get('hash/:input', function (req, res){
-    var hashedString = hash(req.params.input);
+    var hashedString = hash(req.params.input , 'this-is-some-random-string');
     res.send(hashedString);
     
 }); 
