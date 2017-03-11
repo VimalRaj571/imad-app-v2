ff7a0c6875b5d8036c9f94e1d09e17dfc5a6d5e2
Vimal <Vimalraj.ktm95@gmail.com>
2017-03-12 01:30:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.post('/create-user' ,function (req ,res){
    var dbString = hash(password, salt); 
    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)',[username,dbString], function(err,result){
        if(err){
-           res.status(500).send(err.tostring());
+           res.status(500).send(err.toString());
        } else{
            res.send('User Sucessfully created :' + username);
        }
