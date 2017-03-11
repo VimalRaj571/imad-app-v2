59afa9f10c948ca12bc7ece72ccce71672c87798
Vimal <Vimalraj.ktm95@gmail.com>
2017-03-11 19:01:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
+
 
 var config = {
     user : `vimalraj571`,
@@ -59,6 +61,21 @@ app.get('/counter', function (req, res) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
+function hash(input){
+    //How do we create a hash
+    var hashed = crypto.pbkdf2Sync();
+}
+
+app.get('hash/:input', function (req, res){
+    var hashedString = hash(req.params.input);
+    res.send(hashedString);
+    
+}); 
+
+
+
 
 
 //Connection pools
