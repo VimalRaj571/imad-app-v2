34bd3b8460896e3aa7ebf4d48843491aa92ef72d
Vimal <Vimalraj.ktm95@gmail.com>
2017-03-19 05:09:40 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -72,6 +72,7 @@ request.onreadystatechange = function(){
     console.log(username);
     console.log(password);
     request.open('POST', 'http://vimalraj571.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username : username , password : password}));
 
 };
