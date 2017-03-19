f2307ab94ef51848dba0e3eb6b3758e1e868ee89
Vimal <Vimalraj.ktm95@gmail.com>
2017-03-19 05:14:06 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,35 +13,6 @@ img.onclick = function(){                                                       
     var animate = setInterval(moveRight,50); // call moveRight in 20msec
 };
 
-//Counter CODE
-var btn = document.getElementById('btn');
-
-//var counter = 0;
-
-btn.onclick = function(){
-    
-//Create request Obj
-var request = new XMLHttpRequest();
-
-//Capture the response and store in the variable
-
-request.onreadystatechange = function(){
-    if(request.readyState === XMLHttpRequest.DONE){
-        //Take some Action 
-        if(request.status === 200){
-            //Render in the correct span
-            var counter = request.responseText;
-            //counter = counter + 1;
-            var span = document.getElementById('span_no');
-            span.innerHTML = counter.toString();
-            }
-        }
-    };
-    //Make Req
-    request.open('GET', 'http://vimalraj571.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
-
 
 //Submit Username/Password
 
